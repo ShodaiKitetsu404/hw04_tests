7902d3ef9416488480f7e199ea461a646cf177a2
Стас Басов <stas.basov@whatever.com>
2023-01-15 01:05:37 +0800

    fix

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -7,4 +7,4 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
-        help_texts = {'group': 'Группа', 'text': 'Текст поста'}
+        help_texts = {'group': 'Группа', 'text': 'Тестовый пост формы'}
